fd67bd91f2fe10247800e8c065634a2adede5500
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2019-05-15 09:37:26 +0100

    WIP - log a journey id

--- proxy-node-shared/src/main/java/uk/gov/ida/notification/shared/ProxyNodeLoggingFilter.java
+++ proxy-node-shared/src/main/java/uk/gov/ida/notification/shared/ProxyNodeLoggingFilter.java
@@ -1,5 +1,6 @@
 package uk.gov.ida.notification.shared;
 
+import org.apache.http.HttpHeaders;
 import org.slf4j.MDC;
 
 import javax.ws.rs.container.ContainerRequestContext;
@@ -7,26 +8,44 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
-
-import static uk.gov.ida.notification.shared.IstioHeaders.X_B3_TRACEID;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.logging.Level;
 
 @Provider
 public class ProxyNodeLoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
+    private ProxyNodeLogger proxyNodeLogger = new ProxyNodeLogger();
+
     @Override
     public void filter(ContainerRequestContext requestContext) {
-        String istioTraceId = requestContext.getHeaderString(X_B3_TRACEID);
-        if (istioTraceId != null && !istioTraceId.isBlank()) {
-            MDC.put(X_B3_TRACEID, istioTraceId);
-        }
+        proxyNodeLogger.addContext(ProxyNodeMDCKey.PN_JOURNEY_ID, getJourneyId(requestContext));
+        proxyNodeLogger.addContext(ProxyNodeMDCKey.REFERER, requestContext.getHeaderString(HttpHeaders.REFERER));
+        proxyNodeLogger.addContext(ProxyNodeMDCKey.RESOURCE_PATH, getResourcePath(requestContext));
+        proxyNodeLogger.addContext(ProxyNodeMDCKey.INGRESS_MEDIA_TYPE, requestContext.getMediaType().getType());
+        proxyNodeLogger.log(Level.INFO, () -> "ingress");
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        MDC.remove(X_B3_TRACEID);
+        proxyNodeLogger.addContext(ProxyNodeMDCKey.EGRESS_LOCATION, responseContext.getLocation().toString());
+        proxyNodeLogger.addContext(ProxyNodeMDCKey.EGRESS_STATUS, String.valueOf(responseContext.getStatus()));
+        proxyNodeLogger.addContext(ProxyNodeMDCKey.EGRESS_MEDIA_TYPE, responseContext.getMediaType().getType());
+        proxyNodeLogger.log(Level.INFO, () -> "egress");
+
+        String journeyId = getJourneyId(requestContext);
         for (ProxyNodeMDCKey key : ProxyNodeMDCKey.values()) {
             MDC.remove(key.name());
         }
+        responseContext.getHeaders().add(ProxyNodeMDCKey.PN_JOURNEY_ID.name(), journeyId);
+    }
+
+    private String getResourcePath(ContainerRequestContext requestContext) {
+        return requestContext.getUriInfo().getPath();
+    }
+
+    private String getJourneyId(ContainerRequestContext requestContext) {
+        return Objects.requireNonNullElse(requestContext.getHeaderString(ProxyNodeMDCKey.PN_JOURNEY_ID.name()), UUID.randomUUID().toString());
     }
 
 }
